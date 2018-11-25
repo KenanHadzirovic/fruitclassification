efda953545f30d12fca37453736d990aed536032
Kenan Hadzirovic <khadzirovi1@etf.unsa.ba>
2018-11-25 17:49:44 +0100

    modifications

--- applyMask.m
+++ applyMask.m
@@ -1,7 +1,7 @@
-function [ outputImage ] = applyMask( height, width, Xmin, Xmax, Ymin, Ymax )
+function [ outputImage ] = applyMask( originalImage, Xmin, Xmax, Ymin, Ymax )
 %APPLYFILTER Function that applies mask per given coordinates
 %   When given size of image and coordinates, apply mask to the area not
 %   included in Xmin, Xmax, Ymin, Ymax
-
+[height, width, ~] = size(originalImage);
 
 end
